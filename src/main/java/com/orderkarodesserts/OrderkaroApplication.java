dc782b2e91e852270f03602c8b17f8203d507f49
unknown <ratnasreej@gmail.com>
2023-11-02 19:33:53 +0530

    first commit

---
+++ src/main/java/com/orderkarodesserts/OrderkaroApplication.java
@@ -0,0 +1,13 @@
+package com.orderkarodesserts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderkaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderkaroApplication.class, args);
+	}
+
+}
